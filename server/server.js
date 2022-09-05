6f6974b0ab66950794182adce535e1d269e4bfe1
ATMR90 <Art1603@yandex.ru>
2022-09-06 02:36:12 +0300

    server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const fallback = require ('express-history-api-fallback')
 const express = require('express');
 const path = require('path');
 
@@ -6,9 +7,10 @@ const { NODE_ENV } = require('./utils/constants');
 const { DIST_DIR } = require('./utils/constants');
 
 const pathDistDir = path.join(__dirname, '../', DIST_DIR);
-
+console.log(pathDistDir)
 const app = express();
 
 app.use(express.static(pathDistDir));
+app.use(fallback('index.html', { root: pathDistDir }))
 
 app.listen(PORT, () => console.log(`Server http://localhost:${PORT}`+` Path:${pathDistDir}`+` node_env:${NODE_ENV}`));
